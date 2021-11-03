01508eee4956aa6d988ceb3b27c89d251eb17398
stream3715 <stream.larn3@gmail.com>
2021-11-03 23:04:08 +0900

    Add Waseda Lock Read

---
+++ dump/readSpecificArea.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import binascii
+import nfc
+import time
+
+# 学生証のサービスコード
+service_code = 0x100B
+
+
+def on_connect_nfc(tag):
+    # タグのIDなどを出力する
+    # print tag
+
+    if isinstance(tag, nfc.tag.tt3.Type3Tag):
+        try:
+            sc = nfc.tag.tt3.ServiceCode(
+                service_code >> 6, service_code & 0x3f)
+            bc = [
+                nfc.tag.tt3.BlockCode(0, service=0),
+                nfc.tag.tt3.BlockCode(7, service=0),
+                nfc.tag.tt3.BlockCode(8, service=0),
+                nfc.tag.tt3.BlockCode(9, service=0)]
+            data = tag.read_without_encryption([sc], bc)
+            print(data)
+        except Exception as e:
+            print("error: %s" % e)
+    else:
+        print("error: tag isn't Type3Tag")
+
+
+def main():
+    clf = nfc.ContactlessFrontend('usb')
+    while True:
+        clf.connect(rdwr={'on-connect': on_connect_nfc})
+        time.sleep(3)
+
+
+if __name__ == "__main__":
+    main()
